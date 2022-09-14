3c639a659aa8b5ffde0b21bd6914330eb896e947
luiz <luiz.contes@hotmail.com>
2022-09-14 23:56:27 +0100

    odd ocurrencies implemented

---
+++ experiment.js
@@ -0,0 +1,68 @@
+const prompt = require('prompt-sync')({ sigint: true })
+
+const arr = [9, 3, 9, 3, 9, 7, 155855, 9, 7, 4, 4, 5, 8, 5, 8]
+
+function getOdd(a) {
+
+    let list = []
+
+    let number = []
+    for (let i = 0; i < a.length; i++) {
+        if (!list[a[i]]) {
+            list[a[i]] = 1
+        } else {
+            list[a[i]] += 1
+        }
+
+        // if (list[a[i]] % 2 !== 0) {
+        //     number[a[i]] = 1
+        // } 
+        // if (list[a[i]] % 2 === 0) {
+        //     number[a[i]] = 0
+        // }
+    }
+    return list.indexOf(1)
+
+    // console.log(list)
+
+    // console.log(list)
+
+    // for (let z = 0; z < list.length; z++) {
+    //     // console.log(z)
+    //     if (list[z]) {
+    //         if (list[z] % 2 !== 0) {
+    //             return list.indexOf(list[z])
+    //         }
+    //     }
+    // }
+
+
+    // list.push(9)
+    // list.push(7)
+    // list.indexOf(3)
+
+    // return list
+}
+
+console.log(getOdd(arr))
+
+function solution(A) {
+    let list = []
+    let number = []
+
+    for (let i=0; i<A.length; i++) {
+        if(!list[A[i]]) {
+            list[A[i]] = 1
+        } else {
+            list[A[i]] += 1
+        }
+
+        if(list[A[i]]%2 !==0) {
+            number[A[i]] = 1
+        }
+        if(list[A[i]]%2 === 0) {
+            number[A[i]] = 0
+        }
+    }
+    return number.indexOf(1)
+}
--- oddOccurencesInArray.js
+++ oddOccurencesInArray.js
@@ -1,29 +1,28 @@
 const prompt = require('prompt-sync')({ sigint: true })
 
-const arr = [9, 3, 9, 3, 9, 7, 9, 7, 4, 4, 4]
+const arr = [9, 3, 9, 3, 9, 7, 9, 7, 4, 4, 5]
 
 function getOdd(a) {
 
-    let list = []
-    for (let i = 0; i < a.length; i++) {
-        if (list.length === 0) {
-            list.push([a[i]])
-        } else {
-            let include = true
-            for (let j = 0; j < list.length; j++) {
-                if (list[j].includes(a[i])) {
-                    list[j].push(a[i])
-                    include = false
-                    break
-                }
-            }
-            if (include) {
-                list.push([a[i]])
+    let list = [[a[0]]]
+    for (let i = 1; i < a.length; i++) {
+        
+        let include = true
+        for (let j = 0; j < list.length; j++) {
+            if (list[j].includes(a[i])) {
+                list[j].push(a[i])
+                include = false
+                break
             }
         }
+        if (include) {
+            list.push([a[i]])
+        }
+        
     }
-    for (let z=0; z<list.length; z++) {
-        if (list[z].length%2 !== 0) {
+    console.log(list)
+    for (let z = 0; z < list.length; z++) {
+        if (list[z].length % 2 !== 0) {
             return (list[z][0])
         }
     }
